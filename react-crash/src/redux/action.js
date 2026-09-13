9940cc9b24cd9ef2e140193a743cd543cba7ad37
Marcelo Taparelli <133427366+marcelotaparelli@users.noreply.github.com>
2026-09-12 23:29:46 -0300

    Delete react-crash directory

--- react-crash/src/redux/action.js
+++
@@ -1,9 +0,0 @@
-export const changeTabActive = (data) => ({
-    type: 'ACTIVE_TAB',
-    payload: data   
-})
-
-export const changeProjectActive = (info) => ({
-    type: 'ACTIVE_PROJECT',
-    payload: info   
-})
--- react-crash/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore} from "redux";
-import { rootReducer } from '/src/redux/reducer.js';
-
-const store = createStore(rootReducer);
-
-export default store;
